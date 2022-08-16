10fa2553e784c7751990161836e24047b82f6fbc
imdivish <60841433+imdivish@users.noreply.github.com>
2022-08-16 18:00:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// element = document.getElementById("login")
+//     login.style.width = "8vw";
+//     login.style.height = "30px";
+//     element.addEventListener("mouseover", loginjs)
+//     function loginjs() {
+//         if(login.style.width == "8vw"){
+//             login.style.width = "8.3vw";
+//             login.style.height = "35px";
+//         }
+//         else if(login.style.width == "8.3vw"){
+//             login.style.width = "8vw";
+//             login.style.height = "30px";
+//         }
+//     }
+//     element.addEventListener("mouseout", loginjs)
+//     function loginjs() {
+//         if(login.style.width == "8vw"){
+//             login.style.width = "8.3vw";
+//             login.style.height = "35px";
+//         }
+//         else if(login.style.width == "8.3vw"){
+//             login.style.width = "8vw";
+//             login.style.height = "30px";
+//         }
+//     }
+
+
